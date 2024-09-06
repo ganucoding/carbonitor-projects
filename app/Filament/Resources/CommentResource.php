7e78a669e66d02f19343f7763fba45ae79ba80bc
ganucoding <141534453+ganucoding@users.noreply.github.com>
2024-09-06 08:12:21 +0800

    Hide create button for CommentResource

--- app/Filament/Resources/CommentResource.php
+++ app/Filament/Resources/CommentResource.php
@@ -87,4 +87,9 @@ class CommentResource extends Resource
             'edit' => Pages\EditComment::route('/{record}/edit'),
         ];
     }
+
+    public static function canCreate(): bool
+    {
+        return false;
+    }
 }
