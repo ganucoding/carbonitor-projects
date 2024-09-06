41da9ba752089e3c4740cd4013a627f6d413aa11
ganucoding <141534453+ganucoding@users.noreply.github.com>
2024-09-06 15:04:08 +0800

    Generated Filament resources for Country, ProjectStatus & ProjectType. And Renamed table migration files

---
+++ app/Filament/Resources/ProjectStatusResource.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ProjectStatusResource\Pages;
+use App\Filament\Resources\ProjectStatusResource\RelationManagers;
+use App\Models\ProjectStatus;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ProjectStatusResource extends Resource
+{
+    protected static ?string $model = ProjectStatus::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProjectStatuses::route('/'),
+            'create' => Pages\CreateProjectStatus::route('/create'),
+            'edit' => Pages\EditProjectStatus::route('/{record}/edit'),
+        ];
+    }
+}
