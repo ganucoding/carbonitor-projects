8fd6aa853d32cad95d4ef3a70851bf38e65652d5
ganucoding <141534453+ganucoding@users.noreply.github.com>
2024-09-07 15:25:54 +0800

    Re-styled error minimal layout blade file. And updated message (more elaboration regarding the error) for 403.blade.php

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,5 +1,6 @@
 @extends('errors::minimal')
 
-@section('title', __('Forbidden'))
+@section('title', __('Access Denied'))
 @section('code', '403')
-@section('message', __($exception->getMessage() ?: 'Forbidden'))
+@section('message', __('It looks like you haven’t been approved by an admin yet. Please wait for approval to access the
+    admin login.'))
