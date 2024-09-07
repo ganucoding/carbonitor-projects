f1c91e59ae7fc56c3a7a301f0a0dc943b756c6c3
ganucoding <141534453+ganucoding@users.noreply.github.com>
2024-09-07 18:14:06 +0800

    symlink attempt

--- routes/web.php
+++ routes/web.php
@@ -34,3 +34,9 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+Route::get('/link', function () {
+    $target = '/home/username/app_foler_name/storage/app/public';
+    $shortcut = '/home/username/public_html/storage';
+    symlink($target, $shortcut);
+});
