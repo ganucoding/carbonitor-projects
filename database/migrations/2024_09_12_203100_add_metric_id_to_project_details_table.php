370ef607d9a1e2af8e2de1361b5e987274f4f6a6
ganucoding <141534453+ganucoding@users.noreply.github.com>
2024-09-12 20:33:30 +0800

    Added migration add_metric_id_to_project_details_table

---
+++ database/migrations/2024_09_12_203100_add_metric_id_to_project_details_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('project_details', function (Blueprint $table) {
+            $table->foreignId('metric_id')->nullable()->constrained('metrics')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('project_details', function (Blueprint $table) {
+            $table->dropForeign(['metric_id']);
+            $table->dropColumn('metric_id');
+        });
+    }
+};
