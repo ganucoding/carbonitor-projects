7c05fe3f8c34f79051197ab9528cc25d8fa92301
ganucoding <141534453+ganucoding@users.noreply.github.com>
2024-09-12 21:02:05 +0800

    Added migration add_is_approved_and_approved_by_to_comments_table, and Updated models Comment and ProjectDetail

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -9,10 +9,15 @@ class Comment extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['project_id', 'username', 'email', 'comment'];
+    protected $fillable = ['project_id', 'username', 'email', 'comment', 'is_approved', 'approved_by'];
 
     public function project()
     {
         return $this->belongsTo(Project::class);
+    }
+
+    public function approver()
+    {
+        return $this->belongsTo(User::class, 'approved_by');
     }
 }
