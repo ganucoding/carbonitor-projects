e45753e87b3a2b305f05bf4b8e333a73c772cbd9
ganucoding <141534453+ganucoding@users.noreply.github.com>
2024-09-12 20:26:51 +0800

    Generated mvc for Metric

---
+++ app/Http/Controllers/MetricController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Metric;
+use Illuminate\Http\Request;
+
+class MetricController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Metric $metric)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Metric $metric)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Metric $metric)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Metric $metric)
+    {
+        //
+    }
+}
