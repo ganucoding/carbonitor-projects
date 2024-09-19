34baff004a52236e8f8332a2def11a4ba6818236
ganucoding <141534453+ganucoding@users.noreply.github.com>
2024-09-19 21:46:39 +0800

    Added new columns to project_details_table & issuances_table

---
+++ database/migrations/2024_09_19_214019_add_attributes_emission_reduction_and_histories_json_to_issuances_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('issuances', function (Blueprint $table) {
+            $table->boolean('attributes_emission_reduction')->nullable();
+            $table->json('histories_json')->nullable(); // to store 3 columns/fields => credits, symbol & description
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('issuances', function (Blueprint $table) {
+            $table->dropColumn('attributes_emission_reduction');
+            $table->dropColumn('histories_json');
+        });
+    }
+};
